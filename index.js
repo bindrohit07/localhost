ea07ab051aad6f2667817981790eba963cd1b217
Rohit Kumar Bind <rohitbind9@gmail.com>
2023-08-24 10:24:35 +0530

    style added to the page

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const server =http.createServer((req,res) =>{
         <html>
         <body>
         <h2>Hello World!!</h2>
-        <h1>Server connection successfull :) </h1>
+        <h1 style="color: white;text-shadow: 1px 1px 2px black, 0 0 25px blue, 0 0 5px darkblue;">Server connection successfull :) </h1>
         </body>
         </html>
         `);
