7486792587d4a8ae4e98fb142b5b84eeff048870
Rohit Kumar Bind <rohitbind9@gmail.com>
2023-08-25 09:14:09 +0530

    some file added to localhost

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 const http = require("http");
-const hostname ="localhost";
-const port = 3000;
+const fs = require('fs');
+const home=fs.readFileSync("index.html")
+const about=fs.readFileSync("about.html")
+const pages=fs.readFileSync("pages.html")
+const services=fs.readFileSync("services.html")
+const contacts=fs.readFileSync("contacts.html")
+const hostname = '127.0.0.1';
+const port = 3060;
 const server =http.createServer((req,res) =>{
         res.statusCode=200;
-        res.setHeader(`Content-Type`,`text/html`);
-        res.end(`
-        <html>
-        <body>
-        <h2>Hello World!!</h2>
-        <h1 style="color: white;text-shadow: 1px 1px 2px black, 0 0 25px blue, 0 0 5px darkblue;">Server connection successfull :) </h1>
-        </body>
-        </html>
-        `);
+        res.setHeader('Content-Type','text/html');
+        url=req.url;
+        if(url=='/'){
+                res.end(home);
+        }
+        else if (url=='/about') {
+               res.end(about) 
+        }
+        else if (url=='/pages') {
+                res.end(pages) 
+         }
+         else if (url=='/services') {
+                res.end(services) 
+         }
+         else if (url=='/contacts') {
+                res.end(contacts) 
+         }
+         else if (url=='/about') {
+                res.end(about) 
+         }
+      
+
 })
 server.listen(port,hostname,()=>{
         console.log(`server running at http://${hostname}:${port}`);
